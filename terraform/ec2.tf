e7307f0732ee825b62f9a9f65bc4ab95254b6fee
Miguel-Araujo325 <miguel.asilva@sptech.school>
2025-04-17 13:18:11 -0300

    ajustes tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -18,13 +18,19 @@ resource "aws_instance" "ec2-public-pops" {
     destination = "/home/ubuntu/.ssh/key-ec2-private-pops.pem"
   }
 
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 400 /home/ubuntu/.ssh/key-ec2-private-pops.pem"
+    ]
+  }
+
   connection {
     type        = "ssh"
     user        = "ubuntu"
     private_key = file("C:\\keys\\key-ec2-public-pops.pem")
     host        = self.public_ip
 
-    bastion_host        = aws_instance.ec2-private-pops.public_ip
+    bastion_host        = aws_instance.ec2-public-pops.public_ip
     bastion_user        = "ubuntu"
     bastion_private_key = file("C:\\keys\\key-ec2-public-pops.pem")
   }
@@ -40,9 +46,9 @@ resource "aws_instance" "ec2-private-pops" {
   subnet_id              = aws_subnet.private.id
   associate_public_ip_address = false
   security_groups        = [aws_security_group.sg-private-pops.id]
-  key_name = "key-ec2-private-edu-invtt"
+  key_name = "key-ec2-private-pops"
 
-  depends_on = [aws_instance.ec2-private-pops, aws_route_table_association.rt-private-association-pops]
+  depends_on = [aws_instance.ec2-public-pops, aws_route_table_association.rt-private-association-pops]
 
   provisioner "file" {
     source      = "C:\\grupo_pops\\pops-infra\\private.sh"
@@ -60,7 +66,7 @@ resource "aws_instance" "ec2-private-pops" {
     private_key = file("C:\\keys\\key-ec2-private-pops.pem")
     host        = self.private_ip
 
-    bastion_host        = aws_instance.ec2-private-pops.public_ip
+    bastion_host        = aws_instance.ec2-public-pops.public_ip
     bastion_user        = "ubuntu"
     bastion_private_key = file("C:\\keys\\key-ec2-public-pops.pem")
   }
@@ -100,15 +106,15 @@ resource "null_resource" "configurar_bd" {
     bastion_private_key = file("C:\\keys\\key-ec2-public-pops.pem")
   }
 }
-
+/*
 resource "null_resource" "configurar_frontend" {
   depends_on = [aws_instance.ec2-public-pops, aws_instance.ec2-private-pops, null_resource.configurar_bd]
 
   provisioner "remote-exec" {
     inline = [
       "sudo chmod 400 ~/.ssh/key-ec2-private-pops.pem",
-      // "sudo chmod +x /home/ubuntu/public.sh",
-      // "sudo bash /home/ubuntu/public.sh ${aws_instance.ec2-private-edu-invtt.private_ip}"
+      "sudo chmod +x /home/ubuntu/public.sh",
+      "sudo bash /home/ubuntu/public.sh ${aws_instance.ec2-private-edu-invtt.private_ip}"
     ]
   }
 
@@ -119,6 +125,7 @@ resource "null_resource" "configurar_frontend" {
     private_key = file("C:\\keys\\key-ec2-public-pops.pem")
   }
 }
+*/
 
 # ======================== Outputs (para visualizar os IPs) ========================
 output "nginx_public_ip" {
